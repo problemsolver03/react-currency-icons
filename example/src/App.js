cf9ae1db378aa4d5601e2a53bf2fdd6fa3d7553d
problemsolver03 <lnov36630@gmail.com>
2020-08-23 13:58:02 +0530

    fixed icon size issues

--- example/src/App.js
+++ example/src/App.js
@@ -3,7 +3,29 @@ import React from 'react'
 import Currency  from 'react-currency-icons'
 
 const App = () => {
-  return <Currency code="RUB" size="small" />
+  return (
+    <>
+      <Currency code="USD" />
+      <Currency code="INR" />
+      <Currency code="EUR" />
+      <Currency code="GBP" />
+      <Currency code="RUB" />
+      <Currency code="KRW" />
+      <Currency code="CRC" />
+      <Currency code="VND" />
+      <Currency code="AWG" />
+      <Currency code="PYG" />
+      <Currency code="LAK" />
+      <Currency code="ITL" />
+      <Currency code="NGN" />
+      <Currency code="ILS" />
+      <Currency code="TWD" />
+      <Currency code="KZT" />
+      <Currency code="CPY" />
+      <Currency code="JPY" />
+      <Currency code="PHP" />
+    </>
+  )
 }
 
 export default App
